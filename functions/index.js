9b494c8a6b9042afbaab17d29168b1bad8f94362
Anna Tsvitkova <anna.tsvitkova@outright.digital>
2020-09-09 17:54:25 +0300

    Create notification on create appeal

--- functions/index.js
+++ functions/index.js
@@ -9,55 +9,85 @@ const SENDGRID_API_KEY = functions.config().sendgrid.key;
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-exports.sendEmailDeputyForCreate = functions.https.onRequest(async (req, res) => {
-    cors(req, res, async () => {
-        const data = req.body;
+// exports.sendEmailDeputyForCreate = functions.https.onRequest(async (req, res) => {
+//     cors(req, res, async () => {
+//         const data = req.body;
 
-        await admin.auth().createUser({
-            email: data.email,
-            emailVerified: false,
-            password: data.password,
-        })
-        .then(async (userRecord) => {
-            const userRef = admin.firestore().collection('users').doc(userRecord.uid);
+//         await admin.auth().createUser({
+//             email: data.email,
+//             emailVerified: false,
+//             password: data.password,
+//         })
+//         .then(async (userRecord) => {
+//             const userRef = admin.firestore().collection('users').doc(userRecord.uid);
 
-            await userRef.set({
-                email: data.email,
-                name: data.name,
-                role: data.role
-            }).then(() => {
-                const text = data.name + data.email + data.password + userRecord.uid;
+//             await userRef.set({
+//                 email: data.email,
+//                 name: data.name,
+//                 role: data.role,
+//                 surname: data.surname,
+//                 patronymic: data.patronymic
+//             }).then(() => {
+//                 const text = data.name + data.email + data.password + userRecord.uid;
+//                 const msg = {
+//                     to: data.email,
+//                     from: 'annatsvitkova73@gmail.com',
+//                     subject: 'Створено вашу сторiнку',
+//                     text: text,
+//                     html: `<h1>Доброго дня, ${data.name}</h1><strong>Вашу сторiнку було створено на платформi deputy-app. 
+//                     Ваш логин: ${data.email}
+//                     Ваш пароль: ${data.password}</strong>
+//                     <h3><a href='https://us-central1-deputy-app.cloudfunctions.net/confirmEmailDeputy?token=${userRecord.uid}'>Пiдтвердити створення сторiнки</a></h3>`,
+//                 };
+//                 sgMail.send(msg)
+//                 .then(() => {
+//                     res.status(200).send(true);
+//                 });
+//             });
+//         })
+//         .catch(function(error) {
+//             res.status(error.code).send(error.message);
+//         }); 
+//     })
+// })
+
+// exports.confirmEmailDeputy = functions.https.onRequest(async (req, res) => {
+//     cors(req, res, async() => {
+//         const userId = req.query.token;
+
+//         await admin.auth().updateUser(userId, {
+//             emailVerified: true
+//         }).then(async (userRecord) => {
+//             res.redirect("http://localhost:4200/");
+//         }).catch(err => res.status(err.code).send(err.message));
+//     })
+// })
+
+exports.createAppeal = functions.firestore.document('appeals/{appealId}').onCreate( async (snap, context) => {
+        const appeal = snap.data();
+        const userRef = admin.firestore().collection('users').doc(appeal.deputyId);
+        let userEmail;
+        try {
+            await userRef.get().then(span => {
+                userEmail = span.data().email;
+                const text = 'Доброго дня Для Вас було створено новий запит, перегляньте його deputy-app';
                 const msg = {
-                    to: data.email,
+                    to: userEmail,
                     from: 'annatsvitkova73@gmail.com',
-                    subject: 'Створено вашу сторiнку',
+                    subject: 'Створено новий запит',
                     text: text,
-                    html: `<h1>Доброго дня, ${data.name}</h1><strong>Вашу сторiнку було створено на платформi deputy-app. 
-                    Ваш логин: ${data.email}
-                    Ваш пароль: ${data.password}</strong>
-                    <h3><a href='https://us-central1-deputy-app.cloudfunctions.net/confirmEmailDeputy?token=${userRecord.uid}'>Пiдтвердити створення сторiнки</a></h3>`,
+                    html: `<h1>Доброго дня</h1><p>Для Вас було створено новий запит, перегляньте його <a href='https://deputy-app'>deputy-app</a></p>`,
                 };
                 sgMail.send(msg)
                 .then(() => {
-                    res.status(200).send(true);
+                    return true;
                 });
-            });
-        })
-        .catch(function(error) {
-            res.status(error.code).send(error.message);
-        }); 
-    })
-})
-
-
-exports.confirmEmailDeputy = functions.https.onRequest(async (req, res) => {
-    cors(req, res, async() => {
-        const userId = req.query.token;
-
-        await admin.auth().updateUser(userId, {
-            emailVerified: true
-        }).then(async (userRecord) => {
-            res.redirect("http://localhost:4200/");
-        }).catch(err => res.status(err.code).send(err.message));
-    })
-})
+                return userEmail;
+            })
+            
+        } catch (error) {
+            return error;
+        }
+        
+        return true; 
+});
